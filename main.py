8b776ae1a0b778809023e62875edec4ef9fcb267
David Gu <d.gu@sap.com>
2024-06-11 15:42:09 -0700

    Init

---
+++ main.py
@@ -0,0 +1,23 @@
+from langchain_openai import ChatOpenAI
+
+# Initialize the OpenAI LLM with the new API
+llm = ChatOpenAI(
+    model="gpt-3.5-turbo",
+    temperature=0,
+    max_tokens=50,
+    timeout=None,
+    max_retries=2,
+    api_key="sk-proj-6w1ORHzEdyEAVnMkr3AaT3BlbkFJ0EolusFaSxDIaPLxhBFe",
+)
+
+# Invoce the LLM
+messages = [
+    (
+        "system",
+        "You are a helpful assistant that translates English to French. Translate the user sentence.",
+    ),
+    ("human", "I love programming."),
+]
+# ai_msg = llm.invoke(messages)
+# print(ai_msg.content)
+print(messages)
